a221a1c04979326316622c35801de59702989b9e
Libor Vachal <libor.vachal@gmail.com>
2022-10-30 14:27:28 +0100

    feat: spin-up dokku server on droplet

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -17,11 +17,21 @@ locals {
   dokku_hostname = "dokku-do"
 }
 
-resource "digitalocean_droplet" "dokku-server" {
-  image              = "ubuntu-22-10-x64"
-  name               = local.dokku_hostname
-  region             = "fra1"
-  size               = "s-1vcpu-1gb"
-  monitoring         = true
-  ipv6               = false
+resource "digitalocean_droplet" "dokku_server" {
+  image      = "dokku-20-04"
+  name       = local.dokku_hostname
+  region     = "fra1"
+  size       = "s-1vcpu-1gb"
+  monitoring = true
+  ipv6       = false
+  ssh_keys   = [digitalocean_ssh_key.terraform_dokku.fingerprint]
+}
+
+resource "digitalocean_ssh_key" "terraform_dokku" {
+  name       = "Terraform Dokku"
+  public_key = var.digitalocean_ssh_pub_key
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.dokku_server.ipv4_address
 }
--- digitalocean/variables.tf
+++ digitalocean/variables.tf
@@ -2,3 +2,8 @@ variable "digitalocean_api_key" {
   type      = string
   sensitive = true
 }
+
+variable "digitalocean_ssh_pub_key" {
+  type      = string
+  sensitive = true
+}
